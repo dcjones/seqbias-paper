24175db2e3ae0a01da2da0b30ab92a72ba261dee
Daniel Jones <dcjones@cs.washington.edu>
2011-06-12 13:48:03 -0700

    redo freqs figure, etc

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Beacuse the makefile is fucking hopeless.
+
+
+export TEXINPUTS=.:bioinformatics:
+export BSTINPUTS=.:bioinformatics:
+
+TEX=pdflatex
+BIB=bibtex
+
+
+$TEX seqbias-paper.tex
+$BIB seqbias-paper
+$TEX seqbias-paper.tex
+
+
+
